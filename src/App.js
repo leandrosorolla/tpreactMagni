549e1fb3bb33628e027dc6fa4a636f59ad3a3b2b
Leandro Sorolla <leandrosorolla@gmail.com>
2020-04-20 11:01:07 -0300

    modificaciones css rutas y home

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import detalleInstrumento from "./components/detalleInstrumento";
 import Map from "./components/Maps";
+import Productos from "./components/Productos"
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Home}>        
         </Route>
-        <Route path="/home" component={Home}> 
+        <Route path="/Home" component={Home}> 
+        </Route>
+        <Route path="/productos" component={Productos}> 
         </Route>
         <Route path="/detalleInstrumento/:id" component={detalleInstrumento}></Route>{" "}
         <Route path="/map" component={Map}></Route>
